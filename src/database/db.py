1a4122c737344c426251c61073073c3ae179ad43
Evgeniy <evgeniy.sytcevich.glory@gmail.com>
2025-12-08 21:35:24 +0300

    Deploy

---
+++ src/database/db.py
@@ -0,0 +1,9 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+
+engine = create_async_engine(
+    url="postgresql+asyncpg://postgres:postgres@localhost:6432/postgres",
+    pool_size=20,
+    max_overflow=30,
+)
+
+new_session = async_sessionmaker(bind=engine, expire_on_commit=False)
